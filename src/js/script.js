a83fe0640251f4a8bdc1cb5c57593ca75187dd83
Yarik <artamonovy6@gmail.com>
2020-04-16 15:47:07 +0400

    upd

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $('.slider').owlCarousel({
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        items: 1,
+    });
+
+
+
+});
+
+
+
